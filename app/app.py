f1c39984488c038a5dac8985f8b36678fcd5a454
2500030309 <250003039@kluniversity.in>
2026-07-31 21:23:12 +0530

    Dataset Added

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
+import sys
+import os
+# Add project root to Python path so 'src' can be imported
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from flask import Flask, render_template
+from src.data.load_data import load_data , get_summary
+
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -20,5 +28,17 @@ def contact():
 def predict():
     return render_template("predict.html")
 
+@app.route("/dataset")
+def dataset():
+    df = load_data()
+    summary = get_summary(df)
+
+    return render_template(
+        "dataset.html",
+        summary=summary,
+        first_rows = df.head().to_html(index=False)
+    )
+
 if __name__ == "__main__":
     app.run(debug=True)
+
